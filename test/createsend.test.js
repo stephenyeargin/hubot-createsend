2f6880fab5479af08f2839c362f0c01709657a63
Stephen Yeargin <stephen@yearg.in>
2026-03-02 20:33:22 -0600

    Modernize test suite

--- test/createsend.test.js
+++ test/createsend.test.js
@@ -1,12 +1,9 @@
 /* global describe, context it, beforeEach, afterEach */
 
 const Helper = require('hubot-test-helper');
-const chai = require('chai');
-chai.use(require('sinon-chai'));
 const nock = require('nock');
 
 const helper = new Helper('./../src/createsend.js');
-const { expect } = chai;
 
 describe('createsend basic operations', () => {
   let room = null;
@@ -27,7 +24,7 @@ describe('createsend basic operations', () => {
     delete process.env.CREATESEND_LIST_ID;
   });
 
-  context('subscribe a user', () => {
+  describe('subscribe a user', () => {
     beforeEach((done) => {
       nock('https://api.createsend.com')
         .post('/api/v3.3/subscribers/123foo456.json')
@@ -36,14 +33,14 @@ describe('createsend basic operations', () => {
       setTimeout(done, 100);
     });
 
-    it('hubot responds with message', () => expect(room.messages).to.eql([
+    it('hubot responds with message', () => expect(room.messages).toEqual([
       ['alice', '@hubot createsend subscribe johndoe@example.com'],
       ['hubot', '@alice Attempting to subscribe johndoe@example.com...'],
       ['hubot', 'You successfully subscribed johndoe@example.com.'],
     ]));
   });
 
-  context('unsubscribe a user', () => {
+  describe('unsubscribe a user', () => {
     beforeEach((done) => {
       nock('https://api.createsend.com')
         .delete('/api/v3.3/subscribers/123foo456.json?email=johndoe%40example.com')
@@ -52,14 +49,14 @@ describe('createsend basic operations', () => {
       setTimeout(done, 100);
     });
 
-    it('hubot responds with message', () => expect(room.messages).to.eql([
+    it('hubot responds with message', () => expect(room.messages).toEqual([
       ['alice', '@hubot createsend unsubscribe johndoe@example.com'],
       ['hubot', '@alice Attempting to unsubscribe johndoe@example.com...'],
       ['hubot', 'You successfully unsubscribed johndoe@example.com.'],
     ]));
   });
 
-  context('get latest campaign stats', () => {
+  describe('get latest campaign stats', () => {
     beforeEach((done) => {
       nock('https://api.createsend.com')
         .get('/api/v3.3/clients/foobarbaz123456/campaigns.json')
@@ -76,7 +73,7 @@ describe('createsend basic operations', () => {
       setTimeout(done, 100);
     });
 
-    it('hubot responds with message', () => expect(room.messages).to.eql([
+    it('hubot responds with message', () => expect(room.messages).toEqual([
       ['alice', '@hubot createsend'],
       ['hubot', 'Last campaign "Campaign One" was sent to 1000 subscribers (298 opened, 132 clicked, 43 unsubscribed)'],
     ]));
